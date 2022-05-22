0398ed59bc9ec822fb95ad00d4251626a9af1489
George Amberson <George1984Amberson@yandex.ru>
2022-05-22 21:49:57 +0300

    Complete c-style-cast-checker task

--- main.cpp
+++ main.cpp
@@ -12,20 +12,28 @@
 #include <clang/Tooling/Tooling.h>
 #include <clang/Rewrite/Core/Rewriter.h>
 #include <clang/Frontend/CompilerInstance.h>
-
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
+    CastCallBack(Rewriter& rewriter):r(rewriter) {
     };
 
     void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
+        auto E = Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast");
+        auto b = E->getLParenLoc();
+        auto e = E->getRParenLoc();
+        auto est = E->getEndLoc();
+        r.RemoveText(b,1);
+        r.InsertText(b,"static_cast<");
+        r.RemoveText(e,1);
+        r.InsertText(e,">(");
+        r.InsertText(est.getLocWithOffset(1), ")");
     }
+private:
+    Rewriter& r;
 };
 
 class MyASTConsumer : public ASTConsumer {
@@ -49,8 +57,11 @@ public:
     CStyleCheckerFrontendAction() = default;
     
     void EndSourceFileAction() override {
-        rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
-            .write(llvm::outs());
+    SourceManager &SM = rewriter_.getSourceMgr();
+    std::error_code error_code;
+    llvm::raw_fd_ostream outFile("../test/refactor.cpp", error_code, llvm::sys::fs::OF_None);
+    rewriter_.getEditBuffer(SM.getMainFileID()).write(outFile); // --> this will write the result to outFile
+    outFile.close();
     }
 
     std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef /* file */) override {
