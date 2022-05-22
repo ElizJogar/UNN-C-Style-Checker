2fc1cc6806b8d5638a27765578b9ebf4ac36247d
KirillYashin <kerik-yashin@yandex.ru>
2022-05-23 00:08:43 +0300

    fix for workaround script

--- download-and-install-llvm.sh
+++ download-and-install-llvm.sh
@@ -1,8 +1,9 @@
 export UNN_LLVM_TASK_HOME=$PWD
 
-cd /tmp/
+mkdir ./tmp
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz /tmp/
-cd clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz/
+mv clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz ./tmp
+cd tmp
 tar -xvf clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 rm clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 
