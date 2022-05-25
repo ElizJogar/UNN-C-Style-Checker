462057c104e1eafc20063b46e667096c931eef14
Gleb Danshin <glebd79@gmail.com>
2022-05-25 22:01:19 +0300

    Complete c-style-cast-checker task

--- main.cpp
+++ main.cpp
@@ -19,13 +19,35 @@ using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+    CastCallBack(Rewriter& rewriter) : rewriter_(rewriter) {}
 
     void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
+      // Get C style cast expression pointer and check for non-nullptr
+      auto c_style_cast_expr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+      if (c_style_cast_expr) {
+        // Get source location of cast type's left parenthesis
+        auto left_paren = c_style_cast_expr->getLParenLoc();
+        // Get source location of cast type's right parenthesis
+        auto right_paren = c_style_cast_expr->getRParenLoc();
+        // Replace left parenthesis with `static_cast<`
+        rewriter_.ReplaceText(left_paren, 1, "static_cast<");
+        // Replace right parenthesis with `>`
+        rewriter_.ReplaceText(right_paren, 1, ">");
+        // Get location of first end last token
+        // in cast subexpression
+        auto first = c_style_cast_expr->getSubExprAsWritten()->getBeginLoc();
+        auto last = c_style_cast_expr->getSubExprAsWritten()->getEndLoc();
+        // If needed, add parenthesis before first
+        // and after last token in cast subexpression
+        if ('(' != Result.SourceManager->getCharacterData(first)[0]
+          || ')' != Result.SourceManager->getCharacterData(last)[0]) {
+          rewriter_.InsertText(first, "(");
+          rewriter_.InsertTextAfterToken(last, ")");
+        }
+      }
     }
+private:
+    Rewriter& rewriter_;
 };
 
 class MyASTConsumer : public ASTConsumer {
