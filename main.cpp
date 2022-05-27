0848fa251fd04b9f0ee7159d160b3221bcf6220f
Nick Shell <kiwi0tails@gmail.com>
2022-05-27 16:43:02 +0300

    task done

--- main.cpp
+++ main.cpp
@@ -7,69 +7,90 @@
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/ASTMatchers/ASTMatchers.h>
 #include <clang/Basic/Diagnostic.h>
+#include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/FrontendActions.h>
+#include <clang/Rewrite/Core/Rewriter.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
-#include <clang/Rewrite/Core/Rewriter.h>
-#include <clang/Frontend/CompilerInstance.h>
 
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
-public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
 
-    void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
-    }
+private:
+  Rewriter& _rewriter;
+
+public:
+  CastCallBack(Rewriter &rewriter) : _rewriter(rewriter){};
+  void run(const MatchFinder::MatchResult &Result) override {
+
+    const auto *castExpr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+    
+    const auto range = CharSourceRange::getCharRange(
+        castExpr->getLParenLoc(),
+        castExpr->getSubExprAsWritten()->getBeginLoc());
+
+    const auto &sourceManager = *Result.SourceManager;
+
+    const auto type = Lexer::getSourceText(
+        CharSourceRange::getTokenRange(
+            castExpr->getLParenLoc().getLocWithOffset(1),
+            castExpr->getRParenLoc().getLocWithOffset(-1)),
+                             sourceManager, LangOptions());
+
+    const auto *expr = castExpr->getSubExprAsWritten()->IgnoreImpCasts();
+    const auto newText = ("static_cast<" + type + ">(").str();
+    const auto endLoc = Lexer::getLocForEndOfToken(expr->getEndLoc(), 0,
+                                               sourceManager, LangOptions());
+    _rewriter.InsertText(endLoc, ")");
+    _rewriter.ReplaceText(range, newText);
+  }
 };
 
 class MyASTConsumer : public ASTConsumer {
 public:
-    MyASTConsumer(Rewriter &rewriter) : callback_(rewriter) {
-        matcher_.addMatcher(
-                cStyleCastExpr(unless(isExpansionInSystemHeader())).bind("cast"), &callback_);
-    }
+  MyASTConsumer(Rewriter &rewriter) : callback_(rewriter) {
+    matcher_.addMatcher(
+        cStyleCastExpr(unless(isExpansionInSystemHeader())).bind("cast"),
+        &callback_);
+  }
 
-    void HandleTranslationUnit(ASTContext &Context) override {
-        matcher_.matchAST(Context);
-    }
+  void HandleTranslationUnit(ASTContext &Context) override {
+    matcher_.matchAST(Context);
+  }
 
 private:
-    CastCallBack callback_;
-    MatchFinder matcher_;
+  CastCallBack callback_;
+  MatchFinder matcher_;
 };
 
 class CStyleCheckerFrontendAction : public ASTFrontendAction {
 public:
-    CStyleCheckerFrontendAction() = default;
-    
-    void EndSourceFileAction() override {
-        rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
-            .write(llvm::outs());
-    }
+  CStyleCheckerFrontendAction() = default;
 
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef /* file */) override {
-        rewriter_.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-        return std::make_unique<MyASTConsumer>(rewriter_);
-    }
+  void EndSourceFileAction() override {
+    rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
+  }
+
+  std::unique_ptr<ASTConsumer>
+  CreateASTConsumer(CompilerInstance &CI, StringRef /* file */) override {
+    rewriter_.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    return std::make_unique<MyASTConsumer>(rewriter_);
+  }
 
 private:
-    Rewriter rewriter_;
+  Rewriter rewriter_;
 };
 
 static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
 
 int main(int argc, const char **argv) {
-    // Ubuntu 20
-    auto Parser = llvm::ExitOnError()(CommonOptionsParser::create(argc, argv, CastMatcherCategory));
-    // For Ubuntu 18, use the old Clang API to construct the CommonOptionsParser:
-    // CommonOptionsParser Parser(argc, argv, CastMatcherCategory);
+  CommonOptionsParser Parser(argc, argv, CastMatcherCategory);
 
-    ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
-    return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
+  ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
+  return Tool.run(
+      newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
 }
