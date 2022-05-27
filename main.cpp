13c3aac0e347904c05ff3b31dee3bedd93664339
SHVlader <79725044+SHVlader@users.noreply.github.com>
2022-05-27 16:14:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,23 +20,22 @@ using namespace clang::tooling;
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
     CastCallBack(Rewriter& rewriter):_rewriter(rewriter) {
-        // Your code goes here
     };
 
     void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
-        auto cstmtptr = Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast");
+	auto cstmtptr = Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast");
 	auto l_brace = cstmtptr->getLParenLoc();
 	auto r_brace = cstmtptr->getRParenLoc();
-	_rewriter.RemoveText(l_brace,1);
-	_rewriter.RemoveText(r_brace,1);
+	auto end_of_var = Lexer::getLocForEndOfToken(cstmtptr->getSubExprAsWritten()->IgnoreImpCasts()->getEndLoc(), 0, *Result.SourceManager, LangOptions());
+	auto start_of_var = cstmtptr->getSubExprAsWritten()->getBeginLoc();
+	_rewriter.RemoveText(l_brace, 1);
+	_rewriter.ReplaceText(CharSourceRange::getCharRange(r_brace, start_of_var),">");
 	_rewriter.InsertText(l_brace,"static_cast<");
-	_rewriter.InsertText(r_brace,">(");
-	_rewriter.InsertText(r_brace.getLocWithOffset(2),")");
+	_rewriter.InsertText(start_of_var, "(");
+	_rewriter.InsertText(end_of_var, ")");
     }
-private:
+private: 
 Rewriter& _rewriter;
-
 };
 
 class MyASTConsumer : public ASTConsumer {
@@ -84,4 +83,3 @@ int main(int argc, const char **argv) {
     ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
     return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
 }
-
